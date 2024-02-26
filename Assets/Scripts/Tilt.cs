cf174ff69b0a7b89e0f295f41153d3257eba11dc
Rin <rinramz@outlook.com>
2024-02-26 16:01:06 -0600

    Updated for 2 Axis Rotation of the Tray
    Now it can rotate to front and back

--- Assets/Scripts/Tilt.cs
+++ Assets/Scripts/Tilt.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.UI;
@@ -7,24 +8,30 @@ public class Tilt : MonoBehaviour
     public float speed = 10f;
 
     [SerializeField]
-    private InputActionReference left, right;
+    private InputActionReference left, right, front, back;
 
     private void OnEnable()
     {
         left.action.Enable();
         right.action.Enable();
+        front.action.Enable();
+        back.action.Enable();
     }
 
     private void OnDisable()
     {
         left.action.Disable();
         right.action.Disable();
+        front.action.Disable();
+        back.action.Disable();
     }
 
     private void Update()
     {
         float leftTilt = left.action.ReadValue<float>();
         float rightTilt = right.action.ReadValue<float>();
+        float frontTilt = front.action.ReadValue<float>();
+        float backTilt = back.action.ReadValue<float>();
 
         if (leftTilt > 0)
         {
@@ -35,6 +42,16 @@ public class Tilt : MonoBehaviour
         {
             // Tilt right
             transform.Rotate(new Vector3(0, 0, -1) * Time.deltaTime * speed, Space.World);
+        }
+        else if (frontTilt > 0)
+        {
+            //Tilt front
+            transform.Rotate(new Vector3(1, 0, 0) * Time.deltaTime * speed, Space.World);
+        }
+        else if (backTilt > 0)
+        {
+            //Tilt front
+            transform.Rotate(new Vector3(-1, 0, 0) * Time.deltaTime * speed, Space.World);
         }
     }
 }
