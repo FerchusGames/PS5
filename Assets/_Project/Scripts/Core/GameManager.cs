36d9c11de29f8c22316652232ba0c6ec4b076b92
Rin <rinramz@outlook.com>
2024-03-10 18:46:52 -0600

    Updated a lot of stuff

--- Assets/_Project/Scripts/Core/GameManager.cs
+++ Assets/_Project/Scripts/Core/GameManager.cs
@@ -7,11 +7,14 @@ namespace PS5.Core
     public class GameManager : MonoBehaviour
     {
         public static GameManager Instance { get; private set; }
+        public GameStates gameStates;
 
         [field:SerializeField]
         public int CurrentScore { get; private set; }
 
         public event Action<int> onScoreChangeAction;
+        public event Action SaveEvent;
+        public event Action OnLoseEvent; 
         
         private void Awake()
         {
@@ -24,6 +27,8 @@ namespace PS5.Core
             {
                 Destroy(gameObject);
             }
+
+            gameStates = GameStates.gaming;
         }
 
         public void AddScore(int scoreToAdd)
@@ -32,5 +37,27 @@ namespace PS5.Core
             Debug.Log("Current Score: " + CurrentScore);
             onScoreChangeAction?.Invoke(CurrentScore);
         }
+
+        public void Save()
+        {
+            int finalScore = CurrentScore;
+            Debug.Log("Final Score: " + finalScore);
+            PlayerPrefs.SetInt("Score", finalScore);
+            PlayerPrefs.Save();
+            SaveEvent?.Invoke();
+        }
+        
+        public void OnLose()
+        {
+            gameStates = GameStates.lose;
+            OnLoseEvent?.Invoke();
+        }
     }
+}
+
+public enum GameStates
+{
+    lose,
+    pause,
+    gaming
 }
---
+++ Assets/_Project/Scripts/Objects/ObjectsLose.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PS5.Core;
+
+
+namespace PS5.Objects
+{
+    public class ObjectsLose : MonoBehaviour
+    {
+        private int objectsFell;
+        
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Food"))
+            {
+                GameManager.Instance.Save();
+                Destroy(other.gameObject);
+                objectsFell++;
+            }
+        }
+        
+        private void Update()
+        {
+            if (objectsFell > 2)
+            {
+                GameManager.Instance.OnLose();
+            }
+        }
+    }
+}
