490eb1956e4fa8002aa98cef6af622fb18856672
Ricardo Patricio Pérez Maginniss <94476998+PaTo81013@users.noreply.github.com>
2024-04-29 20:53:12 -0600

    clase

---
+++ Assets/_Project/Scenes/Pato/AudioLibrary.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[CreateAssetMenu(fileName="AudioLibrary" ,menuName = "Scriptables/AudioLibrary", order = 0)]
+
+public class AudioLibrary : ScriptableObject
+{
+    [SerializeField] AudioData[] audios;
+
+
+    public AudioClip GetAudio(SOUND_TYPE _soundRequest)
+    {
+        for (int i = 0; i < audios.Length; i++)
+        {
+            if (audios[i].soundType == _soundRequest)
+            {
+                int randomNumber = UnityEngine.Random.Range(0, audios[i].clip.Length);
+                return audios[i].clip[randomNumber];
+            }
+        }
+        return null;
+    }
+
+}
+[Serializable]
+public class AudioData
+{
+    public SOUND_TYPE soundType;
+    public AudioClip[] clip;
+}
+public enum SOUND_TYPE
+{
+    PAPUPASOS,
+    PAPUDISPAROS,
+    PAPUTRAIL,
+    PAPUMUERTE,
+    PAPUSPAWN
+}
