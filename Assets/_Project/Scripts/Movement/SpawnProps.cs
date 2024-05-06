151b667400311617971d1ac09c2f0ea25695de50
Ferchus <98918903+FerchusGames@users.noreply.github.com>
2024-05-05 18:57:59 -0600

    Before adding more props to spline

--- Assets/_Project/Scripts/Movement/SpawnProps.cs
+++ Assets/_Project/Scripts/Movement/SpawnProps.cs
@@ -8,13 +8,21 @@ public class SpawnProps : MonoBehaviour
 {
    [SerializeField] private GameObject[] _propGameObjects;
    [SerializeField] private Transform[] _propPlacements;
+   [SerializeField] private Transform _parent;
 
+   [SerializeField] private float _minScaleMultiplier = 1f;
+   [SerializeField] private float _maxScaleMultiplier = 1f;
+   
    private void Start()
    {
       foreach (Transform propPlacement in _propPlacements)
       {
+         float scaleMultiplier = Random.Range(_minScaleMultiplier, _maxScaleMultiplier);
          int propIndex = Random.Range(0, _propGameObjects.Length);
-         Instantiate(_propGameObjects[propIndex], propPlacement);
+         GameObject prop = Instantiate(_propGameObjects[propIndex], propPlacement);
+         prop.transform.localScale = new Vector3((1 / _parent.localScale.x) * scaleMultiplier, 
+            (1 / _parent.localScale.y) * scaleMultiplier , 
+            (1 / _parent.localScale.z) * scaleMultiplier); 
       }
    }
 }
