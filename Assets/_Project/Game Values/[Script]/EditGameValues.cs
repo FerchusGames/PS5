9f8cbe4f7f4172d02cddd1485d3a9c8c6e3cf5d8
Ferchus <98918903+FerchusGames@users.noreply.github.com>
2024-05-21 22:27:22 -0600

    Developer Menu

--- Assets/_Project/Game Values/[Script]/EditGameValues.cs
+++ Assets/_Project/Game Values/[Script]/EditGameValues.cs
@@ -1,12 +1,59 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using Unity.VisualScripting.Dependencies.NCalc;
 using UnityEngine;
 using UnityEngine.Serialization;
+using UnityEngine.UI;
 
 public class EditGameValues : MonoBehaviour
 {
     [SerializeField] private ValueToAssign _valueToAssign;
+
+    [SerializeField] private TextMeshProUGUI _text;
+    
+    private Slider _slider;
+    
+    private void Awake()
+    {
+        _slider = GetComponent<Slider>();
+    }
+
+    private void Start()
+    {
+        InitializeSliderValues();
+    }
+
+    private void InitializeSliderValues()
+    {
+        if (_slider)
+        {
+            switch (_valueToAssign)
+            {
+                case ValueToAssign.MaxSpeed:
+                    _slider.value = GameManager.Instance.GameValues.MaxSpeed;
+                    break;
+                case ValueToAssign.TiltControlSpeed:
+                    _slider.value = GameManager.Instance.GameValues.TiltControlSpeed;
+                    break;
+                case ValueToAssign.TurningTiltRate:
+                    _slider.value = GameManager.Instance.GameValues.TurningTiltRate;
+                    break;
+                case ValueToAssign.WeightTiltRate:
+                    _slider.value = GameManager.Instance.GameValues.WeightTiltRate;
+                    break;
+                case ValueToAssign.MinObjects:
+                    _slider.value = GameManager.Instance.GameValues.MinObjects;
+                    break;
+                case ValueToAssign.MaxObjects:
+                    _slider.value = GameManager.Instance.GameValues.MaxObjects;
+                    break;
+            }
+        }
+
+        _text.text = $"{_slider.value:0.00}";
+    }
     
     public void ChangeGameValue(float value)
     {
@@ -31,6 +78,8 @@ public class EditGameValues : MonoBehaviour
                 GameManager.Instance.GameValues.MaxObjects = (int)value;
                 break;
         }
+        
+        _text.text = $"{value:0.00}";
     }
     
     public enum ValueToAssign
