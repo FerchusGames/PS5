063916cd3657263107f693af25a74abca06b9799
Ferchus <98918903+FerchusGames@users.noreply.github.com>
2024-05-22 19:32:34 -0600

    Changed default Game Values

--- Assets/_Project/Game Values/[Script]/GameValues.cs
+++ Assets/_Project/Game Values/[Script]/GameValues.cs
@@ -13,13 +13,13 @@ public class GameValues : ScriptableObject
 
     [Header("Control")]
     [Range(0.1f, 20f), Tooltip("The speed at which the tray rotates when the player moves the joysticks")]
-    public float TiltControlSpeed = 10f;
+    public float TiltControlSpeed = 2f;
     
     [Header("Tray Balancing")]
     [Range(0.1f, 20f), Tooltip("The rate at which the tray will tilt left/right when the player is turning")]
     public float TurningTiltRate = 5f;
     [Range(0.1f, 20f), Tooltip("The rate at which the tray will tilt because of the objects' weight")]
-    public float WeightTiltRate = 10f;
+    public float WeightTiltRate = 6f;
     
     [Header(("Spawn"))] 
     [Range(4f, 10f), Tooltip("The minimum amount of objects that can spawn in the tray")] 
